a9105a353ca9154c3dbec7dbd2a94891c4354cd1
Della Dominic <delladominic33@gmail.com>
2025-06-09 23:53:09 +0530

    Add Palindrome

---
+++ 02.Basics/03.LeetcodeBasics/09_palindrome.js
@@ -0,0 +1,17 @@
+const isPalindrome = function (x) {
+  if (x < 0) {
+    return false;
+  }
+  let oldX = x;
+  let reverse = 0;
+  while (x !== 0) {
+    lastDigit = x % 10;
+    reverse = reverse * 10 + lastDigit;
+    x = Math.floor(x / 10);
+  }
+  return reverse === oldX;
+};
+
+console.log(isPalindrome(121));
+console.log(isPalindrome(1021));
+console.log(isPalindrome(10));
